d7bd77fc0051180c0a51317975b5d5b019917183
Ty Lippe <tylippe94@gmail.com>
2020-01-23 13:41:49 -0500

    rps is testing OK

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,9 +1,31 @@
 #!/usr/bin/python
 
 import sys
-
+    
 def rock_paper_scissors(n):
-  pass 
+  #create a list for rps
+  rps = ['rock', 'paper', 'scissors']
+  #blank list to hold possible plays
+  poss = []
+  #base case
+  if n <= 0:
+    return [[]]
+  #build a helper function that will use recursion
+  def round(left, results):
+    #base case
+    if left == 0:
+      #add data to poss
+      poss.append(results)
+      return poss
+    else:
+      #loop through list
+      for i in range(0, len(rps)):
+        #recursion  time, subtract one more round, send each rps[i]
+        round(left-1, results + [rps[i]])
+  #call out helper function
+  round(n, [])
+  #return list
+  return poss
 
 
 if __name__ == "__main__":
